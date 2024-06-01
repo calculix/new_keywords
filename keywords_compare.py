c20b83080c7ca387397a52b3b442055fe88a5700
Ihor Mirzov <imirzov@gmail.com>
2024-06-01 12:34:55 +0200

    filtr2

--- keywords_compare.py
+++ keywords_compare.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""© Ihor Mirzov, 2021
+"""© Ihor Mirzov, 2021-2024
 Distributed under GNU General Public License v3.0
 
 Shows the history of the keywords evolution
@@ -12,7 +12,7 @@ import os
 import re
 
 
-def filtr():
+def filtr1():
     """Removes keyword duplicates in a file."""
 
     # file_name = 'keywords_abaqus_2020.txt'
@@ -30,6 +30,24 @@ def filtr():
 
     # with open(file_name, 'w') as f:
     #     f.writelines(lines)
+    pass
+
+
+def filtr2():
+    """Find all uppercase lines in a file."""
+
+    file_name = 'keywords_abaqus_2023.txt'
+    keywords = []
+    with open(file_name, 'r') as f:
+        for l in f.readlines():
+            if l and l == l.upper():
+                keywords.append(l)
+    keywords = sorted(set(keywords))
+    for l in keywords:
+        print(l.strip())
+    print(len(keywords))
+    # with open(file_name, 'w') as f:
+    #     f.writelines(keywords)
     pass
 
 
@@ -95,5 +113,6 @@ if __name__ == '__main__':
     # Clean screen
     os.system('cls' if os.name=='nt' else 'clear')
 
-    # filtr()
+    # filtr1()
+    # filtr2()
     compare()
